bb0cb500a9310ff59215d8d8ebd2ef00df7180ec
Reforced <reforced@hotmail.nl>
2018-11-01 14:15:16 +0100

    Update cms.user.js

--- extensions/cms.user.js
+++ extensions/cms.user.js
@@ -11,6 +11,6 @@
 $(document).ready(function() {
     setTimeout(function() {
         console.log('CMS Loaded');
-        $.getScript('https://chilloutmusica.github.io/cms/cms.js');
+        $.getScript('https://reforced.github.io/cms/cms.js');
     }, 500);
 });
